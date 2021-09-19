2c35f645d5daeb7e9346054ec498215724977d3f
Jason <jasonli.0616@outlook.com>
2021-09-19 00:28:34 -0400

    Add chart

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,12 +1,17 @@
 var labels = [], data = [], colours = [];
-console.log(budget);
-for (let item in budget['items']) {
-    colours.push('rgb(255, 99, 132');
-    data.push(budget['items']['item']);
-    labels.push(item);
+
+const genRandomColour = () => {
+    const n1 = Math.floor(Math.random() * 255);
+    const n2 = Math.floor(Math.random() * 255);
+    const n3 = Math.floor(Math.random() * 255);
+    return `rgb(${n1}, ${n2}, ${n3})`
 }
 
-
+for (let item in budget['items']) {
+    colours.push(genRandomColour());
+    data.push(budget['items'][item]);
+    labels.push(item);
+}
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
